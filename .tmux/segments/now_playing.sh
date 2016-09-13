2f3157d6cfb55a140d8a6e5d32d19e8bce5cccdf
Stefen Ramirez <Stfnrmz0@gmail.com>
2016-09-13 18:58:23 -0500

    Add new tmux files

---
+++ .tmux/segments/now_playing.sh
@@ -0,0 +1,52 @@
+# Print current playing song in your music player of choice.
+source "${TMUX_POWERLINE_DIR_LIB}/text_roll.sh"
+
+TMUX_POWERLINE_SEG_NOW_PLAYING_MUSIC_PLAYER="spotify"
+TMUX_POWERLINE_SEG_NOW_PLAYING_MAX_LEN="25"
+TMUX_POWERLINE_SEG_NOW_PLAYING_TRIM_METHOD="roll"
+TMUX_POWERLINE_SEG_NOW_PLAYING_ROLL_SPEED="5"
+TMUX_POWERLINE_SEG_NOW_PLAYING_NOTE_CHAR="♫"
+
+run_segment() {
+	if [ -z "$TMUX_POWERLINE_SEG_NOW_PLAYING_MUSIC_PLAYER" ]; then
+		return 1
+	fi
+
+	local np
+	case "$TMUX_POWERLINE_SEG_NOW_PLAYING_MUSIC_PLAYER" in
+		"spotify")  np=$(__np_spotify) ;;
+		*)
+			echo "Unknown music player type [${TMUX_POWERLINE_SEG_NOW_PLAYING_MUSIC_PLAYER}]";
+			return 1
+	esac
+	local exitcode="$?"
+	if [ "${exitcode}" -ne 0 ]; then
+		return ${exitcode}
+	fi
+	if [ -n "$np" ]; then
+		case "$TMUX_POWERLINE_SEG_NOW_PLAYING_TRIM_METHOD" in
+			"roll")
+				np=$(roll_text "${np}" ${TMUX_POWERLINE_SEG_NOW_PLAYING_MAX_LEN} ${TMUX_POWERLINE_SEG_NOW_PLAYING_ROLL_SPEED})
+				;;
+			"trim")
+				np=${np:0:TMUX_POWERLINE_SEG_NOW_PLAYING_MAX_LEN}
+				;;
+		esac
+		echo "${np}"
+	fi
+	return 0
+}
+
+__np_spotify() {
+    metadata=$(playerctl metadata)
+
+	if [[ "$?" -eq 0 && -n "$metadata" ]]; then
+		state=$(playerctl status)
+		if [[ $state == "Playing" ]]; then
+			artist=$(playerctl metadata artist)
+			track=$(playerctl metadata title)
+			np=$(echo "${artist} - ${track}")
+		fi
+	fi
+	echo "$np"
+}
